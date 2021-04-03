cd7db7fdddb0bfe41ddbc7fc6dde4f11c7bb23e2
nnakhoa2310 <>
2021-04-03 12:36:50 +0000

    test

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-class Person:
-  def __init__(self,fname,lname):
-    self.fname = fname
-    self.lname = lname
-  def myprint(self):
-    print('my name is ', self.fname, self.lname)
-class Me(Person):
-  def __init__(self,fname,lname):
-    super().__init__(fname,lname)
-    self.gender = 'boy'
-  def printforme(self):
-    print('mu name is', self.fname, self.lname, 'and my love is a', self.gender)
-x = Me('khoa','deptrai')
-x.printforme()
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
 
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
